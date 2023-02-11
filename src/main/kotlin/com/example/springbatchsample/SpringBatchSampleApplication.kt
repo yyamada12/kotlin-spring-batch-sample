253522e56895581c317e2ee54c0a3c4bad9d2076
yyamada12 <12yacropolisy@gmail.com>
2023-02-11 23:41:17 +0900

    create project with spring initializr

---
+++ src/main/kotlin/com/example/springbatchsample/SpringBatchSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbatchsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatchSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBatchSampleApplication>(*args)
+}
